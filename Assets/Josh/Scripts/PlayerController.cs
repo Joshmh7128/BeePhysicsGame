a9044dc8da1f6be2cb18f85dcda1f43109922100
Josh <joshmh7128@gmail.com>
2021-11-15 15:46:17 -0500

    groundwork laid, importing new input system

--- Assets/Josh/Scripts/PlayerController.cs
+++ Assets/Josh/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Main Control Values")]
+    // player variable
+    [SerializeField] CharacterController characterController; // our main character controller that we will be moving
+    Vector3 move;  // our player movement vector
+    [SerializeField] float speed;   // speed of our player
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // get our X and Z movement based off of our left stick
+        float moveH = Input.GetAxis("Horizontal"); 
+        float moveV = Input.GetAxis("Vertical"); 
+
+        // total move 
+        move = new Vector3();
+
+        // apply to the character controller
+        characterController.Move(move * Time.deltaTime * speed);
     }
 }
