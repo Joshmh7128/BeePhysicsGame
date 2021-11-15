55e88a9d7004446c8009c33bac700638a420dade
Josh <joshmh7128@gmail.com>
2021-11-15 15:56:49 -0500

    Basic Bee movement added

--- Assets/Josh/Scripts/PlayerController.cs
+++ Assets/Josh/Scripts/PlayerController.cs
@@ -1,30 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Main Control Values")]
     // player variable
-    [SerializeField] CharacterController characterController; // our main character controller that we will be moving
-    Vector3 move;  // our player movement vector
-    [SerializeField] float speed;   // speed of our player
+    [SerializeField] CharacterController characterController;   // our main character controller that we will be moving
+    Vector3 move;                                               // our player movement vector
+    [SerializeField] float speed;                               // speed of our player
+    Gamepad ourGamepad;                                         // our current gamepad
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // make sure we set our current gamepad
+        ourGamepad = Gamepad.current;
     }
 
     // Update is called once per frame
     void Update()
     {
         // get our X and Z movement based off of our left stick
-        float moveH = Input.GetAxis("Horizontal"); 
-        float moveV = Input.GetAxis("Vertical"); 
+        float moveHx = ourGamepad.leftStick.x.ReadValue();
+        float moveHy = ourGamepad.leftStick.y.ReadValue();
 
         // total move 
-        move = new Vector3();
+        move = new Vector3(moveHx, 0, moveHy);
 
         // apply to the character controller
         characterController.Move(move * Time.deltaTime * speed);
