4810b0324962f796be5919457d76c9723fa932fd
Josh <joshmh7128@gmail.com>
2021-11-21 12:47:13 -0500

    Target Orb Moves around

--- Assets/Josh/Scripts/GenManager.cs
+++ Assets/Josh/Scripts/GenManager.cs
@@ -7,6 +7,7 @@ public class GenManager : MonoBehaviour
     [SerializeField] List<RandomChildSelector> randomChildSelectors; // our list of child selectors
     [SerializeField] Transform targetOrbTransform; // the target orb transform that we want to more around the map
     [SerializeField] float xVariance, yVariance, zVariance; // the amount of randomization we want in our orb placement
+    [SerializeField] bool multiGen; // do we want to generate multiple times in the same level?
 
     // generate the map at the start of the game
     private void Start()
@@ -16,14 +17,17 @@ public class GenManager : MonoBehaviour
 
     public void RegenMap()
     {
-        // for each random child selector on the map, regen the map
-        foreach(RandomChildSelector randomChildSelector in randomChildSelectors)
+        if (multiGen)
         {
-            randomChildSelector.Regen();
+            // for each random child selector on the map, regen the map
+            foreach (RandomChildSelector randomChildSelector in randomChildSelectors)
+            {
+                randomChildSelector.Regen();
+            }
         }
 
         // randomize the position of our target orb transform based on our randomization floats
-        targetOrbTransform.position = new Vector3(Random.Range(-xVariance, xVariance), Random.Range(-yVariance, yVariance), Random.Range(-zVariance, zVariance));
+        targetOrbTransform.position = transform.position + new Vector3(Random.Range(-xVariance, xVariance), Random.Range(-yVariance, yVariance), Random.Range(-zVariance, zVariance));
     }
 
     private void OnDrawGizmos()
