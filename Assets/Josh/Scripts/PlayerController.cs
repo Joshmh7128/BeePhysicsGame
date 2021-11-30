f8eb3b7cb0e342c9d0ee8ca9ed37ab67226304ac
Josh <joshmh7128@gmail.com>
2021-11-29 19:01:26 -0500

    Working on all the different mechanical pieces

--- Assets/Josh/Scripts/PlayerController.cs
+++ Assets/Josh/Scripts/PlayerController.cs
@@ -10,12 +10,12 @@ public class PlayerController : MonoBehaviour
     // player variable
     [SerializeField] CharacterController characterController;   // our main character controller that we will be moving
     Vector3 move;                                               // our player movement vector
-    [SerializeField] float speed;                               // speed of our player
+    [SerializeField] float speed, lowSpeed, highSpeed;                               // speed of our player
     [SerializeField] float rotationSpeed;                            // rotation speed
     float currentRot;                                           // our current rotation on the Y axis
     Gamepad ourGamepad;                                         // our current gamepad
     [SerializeField] LineRenderer renderer1, renderer2, renderer3, renderer4; // our line renderers
-    [SerializeField] Transform lineTarget1, lineTarget2, lineTarget3, lineTarget4; // our line renderer target
+    [SerializeField] Transform lineOrigin, lineTarget1, lineTarget2, lineTarget3, lineTarget4; // our line renderer target
     [SerializeField] Transform movementArrow; // cosmetic arrow representing our movement
     bool startPressed = false;
 
@@ -36,6 +36,17 @@ public class PlayerController : MonoBehaviour
         // get our triggers and use them for vertical movement
         float moveYr = ourGamepad.rightTrigger.ReadValue(); 
         float moveYl = ourGamepad.leftTrigger.ReadValue(); 
+
+        if (ourGamepad.leftTrigger.ReadValue() > 0.5f && ourGamepad.rightTrigger.ReadValue() > 0.5f)
+        {
+            speed = highSpeed;
+        }
+
+        if (ourGamepad.leftTrigger.ReadValue() < 0.5f && ourGamepad.rightTrigger.ReadValue() < 0.5f)
+        {
+            speed = lowSpeed;
+        }
+
         // turn those values in to a movement vector
         move = transform.right * moveHx + transform.forward * moveHy + new Vector3(0, moveYr + -moveYl, 0);
         // apply to the character controller
@@ -46,7 +57,7 @@ public class PlayerController : MonoBehaviour
         // add our rotation to the transform rotation
         transform.rotation = Quaternion.Euler(new Vector3(0, currentRot * rotationSpeed, 0));
         // update the positions of our line renderers
-        renderer1.SetPosition(0, transform.position); renderer2.SetPosition(0, transform.position); renderer3.SetPosition(0, transform.position); renderer4.SetPosition(0, transform.position);
+        renderer1.SetPosition(0, lineOrigin.position); renderer2.SetPosition(0, lineOrigin.position); renderer3.SetPosition(0, lineOrigin.position); renderer4.SetPosition(0, lineOrigin.position);
         renderer1.SetPosition(1, lineTarget1.position); renderer2.SetPosition(1, lineTarget2.position); renderer3.SetPosition(1, lineTarget3.position); renderer4.SetPosition(1, lineTarget4.position);
         // update our movement arrow
         movementArrow.transform.LookAt(movementArrow.transform.position + move);
