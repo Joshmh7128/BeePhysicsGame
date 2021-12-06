279327b31192c1678dbe370c9ba5e23c714685fe
Rook <rookxliu@gmail.com>
2021-12-06 18:55:13 -0500

    Menu done [minus what scenes to target]

--- Assets/Rook/UI Assets/LogoMove.cs
+++ Assets/Rook/UI Assets/LogoMove.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class LogoMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
 
-    // Update is called once per frame
+    public GameObject Target;
+
+
     void Update()
     {
-        
+        // Define a target position above and behind the target transform
+       // Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, Target.transform.position, ref velocity, smoothTime);
     }
+
 }
+
